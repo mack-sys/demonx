2cf3e8b07843c8b3f5e0875207883ba35f236fe8
Mahesh <maxg8@LAPTOP-L6O39Q66>
2024-11-10 00:21:55 +0530

    added new demon service interface

---
+++ src/main/java/com/example/live/serviceinterface/DemonUserServiceInterface.java
@@ -0,0 +1,12 @@
+package com.example.live.serviceinterface;
+
+import java.util.List;
+
+import com.example.live.entity.DemonUser;
+
+public interface DemonUserServiceInterface {
+	
+	public void save(DemonUser demonUser);
+	public List<DemonUser> getAllDemonUser();
+
+}
