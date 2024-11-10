5c9c8cf6cb6c149cae71a4cbd5ceabff1305f2c5
Mahesh <maxg8@LAPTOP-L6O39Q66>
2024-11-11 00:17:05 +0530

    corrected error message

--- src/main/java/com/example/live/serviceinterface/DemonUserServiceInterface.java
+++ src/main/java/com/example/live/serviceinterface/DemonUserServiceInterface.java
@@ -9,9 +9,10 @@ public interface DemonUserServiceInterface {
 	//save data
 	public void save(DemonUser demonUser);
 	
-	//getting all data from demon user
+	
 	public void saveDemonUser(DemonUser demonUser);
 	
-	public List<DemonUser> getAllDemonUser()
+	//getting all data from demon user
+	public List<DemonUser> getAllDemonUser();
 
 }
